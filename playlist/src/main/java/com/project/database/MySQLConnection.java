231a6eb341ea1498df4133e05e544ce2ab1d7cfc
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2020-08-15 12:53:12 -0500

    Added SQL Connection

---
+++ playlist/src/main/java/com/project/database/MySQLConnection.java
@@ -0,0 +1,95 @@
+package com.project.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.project.model.User;
+
+public class MySQLConnection {
+
+	// Checks Database to see if a username already exists.
+	public boolean checkUserName(String username) {
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PlaylistSQL", "root",
+					"codingroot1!");
+			Statement stmt = con.createStatement();
+			String query = "Select UserName From UserTable Where UserName = " + "'" + username + "';";
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				String found = rs.getString("UserName");
+				if (found.equals(username)) {
+					return true;
+				}
+			}
+			con.close();
+		} catch (Exception e) {
+			System.out.println("The Check Username Function Did Not Work");
+			System.out.println(e);
+			return true;
+		}
+
+		return false;
+	}
+
+	// Insert new user information into the database.
+	public void insertNewUser(User newUser) {
+		try {
+			// Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PlaylistSQL", "root",
+					"codingroot1!");
+			Statement stmt = con.createStatement();
+			String firstName = newUser.getFirstName();
+			String lastName = newUser.getLastName();
+			String userName = newUser.getUserName();
+			String passWord = newUser.getPassword();
+			String favoriteArtist = newUser.getFavoriteArtist();
+			String query = "Insert into UserTable Values( " + "'" + firstName + "', " + "'" + lastName + "'," + "'"
+					+ userName + "', " + "'" + passWord + "'," + "'" + favoriteArtist + "');";
+			stmt.execute(query);
+			con.close();
+		} catch (Exception e) {
+			System.out.println("It did not work");
+			System.out.println(e);
+		}
+	}
+
+	// Checks for user by username and password.
+	public User loginUser(String username, String password) {
+		User loggedIn = null;
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PlaylistSQL", "root",
+					"codingroot1!");
+			Statement stmt = con.createStatement();
+			String query = "Select * From UserTable Where UserName = " + "'" + username + "'" + " AND PassWord = '"
+					+ password + "';";
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				String foundUserName = rs.getString("UserName");
+				if (foundUserName.equals(username)) {
+					String foundPassWord = rs.getString("PassWord");
+					if (foundPassWord.equals(password)) {
+						String firstName = rs.getString("FirstName");
+						String lastName = rs.getString("LastName");
+						String favoriteArtist = rs.getString("FavoriteArtist");
+						loggedIn = new User(firstName, lastName, username, password, favoriteArtist);
+						break;
+					} else {
+						System.err.println("\nIncorrect Password!!\n");
+						return null;
+					}
+				}
+			}
+			con.close();
+		} catch (Exception e) {
+			System.out.println("The Check Username Function Did Not Work");
+			System.out.println(e);
+			return null;
+		}
+
+		return loggedIn;
+	}
+}
--- playlist/src/main/java/com/project/methods/MenuAction.java
+++ playlist/src/main/java/com/project/methods/MenuAction.java
@@ -1,30 +1,34 @@
 package com.project.methods;
 
+import com.project.database.MySQLConnection;
 import com.project.datastructure.UserLinkedList;
 import com.project.menu.UserInputs;
 import com.project.model.User;
 
 public class MenuAction {
-	
+
 	static UserLinkedList userDB;
-	
+	static MySQLConnection myDB;
+
 	public MenuAction() {
 		userDB = new UserLinkedList();
+		myDB = new MySQLConnection();
 	}
-	
-	
-	//Method For Login Then Returns User
+
+	// Method For Login Then Returns User
 	public User login() {
 		User loginUser = null;
 		System.out.println("*** LOGIN ***");
 		String userName = UserInputs.getUsername();
 		String passWord = UserInputs.getPassword();
-		loginUser = userDB.listToLogin(userName, passWord);
-		if(loginUser == null) {System.err.println("Incorrect Username Or Password");}
-		return loginUser;	
+		loginUser = myDB.loginUser(userName, passWord);
+		if (loginUser == null) {
+			System.err.println("Incorrect Username Or Password");
+		}
+		return loginUser;
 	}
-	
-	//Method For Registration Then Returns User
+
+	// Method For Registration Then Returns User
 	public User register() {
 		User registerUser = null;
 		String userName = "";
@@ -32,25 +36,27 @@ public class MenuAction {
 		System.out.println("*** REGISTRATION ***");
 		String firstName = UserInputs.getFirstName();
 		String lastName = UserInputs.getLastName();
-		while(checkUserName) {
+		while (checkUserName) {
 			userName = UserInputs.getUsername();
-			checkUserName = userDB.existingUsername(userName);
-			if(checkUserName) {System.err.println("WARNING: Username Exists");}
+			checkUserName = myDB.checkUserName(userName);
+			if (checkUserName) {
+				System.err.println("WARNING: Username Exists");
+			}
 		}
 		String passWord = UserInputs.getPassword();
 		String favoriteArtists = UserInputs.getFavoriteArtist();
 		registerUser = new User(firstName, lastName, userName, passWord, favoriteArtists);
-		userDB.insertTail(registerUser);
+		myDB.insertNewUser(registerUser);
 		return registerUser;
 	}
-	
-	//Method To View All Users In The List -- THIS WILL BE REMOVED LATER
+
+	// Method To View All Users In The List -- THIS WILL BE REMOVED LATER
 	public void viewAll() {
 		System.out.println("*** ALL USERS ***");
 		userDB.showAll();
 	}
-	
-	//This Closes The Program
+
+	// This Closes The Program
 	public void closeProgram() {
 		System.out.println("\nClosing Playlist Program......");
 		UserInputs.closeScanner();
